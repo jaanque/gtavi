981363cbdd748d4f91e6c6ff83648ee8b7468f0c
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2025-11-09 22:54:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,29 +25,39 @@ export default async function Home() {
   const targetDate = await getTargetDate()
 
   return (
-    <div
-      className="relative min-h-screen w-full bg-cover bg-center"
-      style={{
-        backgroundImage: "url('https://www.rockstargames.com/gta-vi/2/_next/dist/images/jason-and-lucia-02-phone-b.jpg')",
-      }}
-    >
-      <div className="absolute inset-0 bg-black opacity-50"></div>
-      <main className="relative z-10 flex min-h-screen flex-col items-center justify-between p-4">
-        {/* Top Spacer */}
-        <div className="w-full h-16"></div>
-
-        {/* Center Content */}
-        <div className="flex flex-col items-center text-center">
-          <h1 className="font-gta text-6xl text-gta-pink sm:text-7xl md:text-8xl" style={{ textShadow: '2px 2px 4px #000' }}>
-            GTA VI
-          </h1>
-          <p
-            className="font-gta text-3xl text-white sm:text-4xl md:text-5xl"
-            style={{ textShadow: '2px 2px 4px #000' }}
-          >
-            COUNTDOWN
-          </p>
-          <Countdown targetDate={targetDate} />
+    (<div className="flex min-h-screen flex-col items-center justify-center">
+      <main className="flex flex-grow items-center justify-center w-full p-12">
+        <div
+          className="flex flex-col lg:flex-row items-center justify-around w-full space-y-8 lg:space-y-0 lg:space-x-8"
+        >
+          {/* Ad Content Left */}
+          <div className="order-2 lg:order-1 flex flex-col items-center text-center">
+            <div className="mb-8">
+              <iframe src="/ad1.html" width="160" height="600" frameBorder="0" scrolling="no"></iframe>
+            </div>
+          </div>
+          {/* Center Content */}
+          <div className="flex-grow order-1 lg:order-2 flex flex-col items-center text-center">
+            <h1
+              className="text-6xl sm:text-7xl md:text-8xl font-bold mb-4 text-gta-pink"
+              style={{ textShadow: '2px 2px 4px #000' }}
+            >
+              GTA VI
+            </h1>
+            <p
+              className="text-3xl sm:text-4xl md:text-5xl mb-8 text-white"
+              style={{ textShadow: '2px 2px 4px #000' }}
+            >
+              COUNTDOWN
+            </p>
+            <Countdown targetDate={targetDate} />
+          </div>
+          {/* Ad Content Right */}
+          <div className="order-3 lg:order-3 flex flex-col items-center text-center">
+            <div>
+              <iframe src="/ad2.html" width="160" height="600" frameBorder="0" scrolling="no"></iframe>
+            </div>
+          </div>
         </div>
 
         {/* Bottom Content */}
